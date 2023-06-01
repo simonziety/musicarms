812c11730a6fa30b7858ecbd5d893d3ad4a8aeef
simonziety <99574658+simonziety@users.noreply.github.com>
2023-06-01 11:19:03 +0700

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const { src, dest, watch, parallel } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const minifyCSS = require("gulp-csso");
+function css() {
+return src("assets/scss/*.scss")
+.pipe(sass())
+.pipe(minifyCSS())
+.pipe(dest("css"));
+}
+function watchFiles() {
+watch("./assets/scss/**/*", css);
+}
+exports.css = css;
+exports.watch = watch;
+exports.default = parallel(css, watchFiles);
